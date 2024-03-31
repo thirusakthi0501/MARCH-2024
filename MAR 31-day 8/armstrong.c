fe900de75aa6d14db81b0d7d6adc504583dc7ce4
thirusakthi0501 <104378608+thirusakthi0501@users.noreply.github.com>
2024-03-31 23:34:10 +0530

    Update and rename palindrome.c to armstrong.c

--- MAR 31-day 8/armstrong.c
+++ MAR 31-day 8/armstrong.c
@@ -2,19 +2,19 @@
 #include<math.h>
 int main()
 {
-    int a,d=0,i,s=0,t;
-    scanf("%d",&a);
-    for(i=a;i>0;i=i/10)
+    int n,d=0,i,sum=0,t;
+    scanf("%d",&n);
+    for(i=n;i>0;i=i/10)
     {
         d++;
     }
     t=n;
-    while(a>0)
+    while(n>0)
     {
-        s=s+pow(a%10,d);
+        sum=sum+pow(n%10,d);
         n=n/10;
     }
-    if(t==s)
+    if(t==sum)
     {
         printf("Armstrong number");
     }
