b33eb1872dbefa3fea9bbd76864cee4107c673d9
thirusakthi0501 <104378608+thirusakthi0501@users.noreply.github.com>
2024-04-07 21:41:32 +0530

    Create pos,neg,zero.c

---
+++ APR 05-day 12/pos,neg,zero.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int q;
+    scanf("%d ",&q);
+    if(q==0)
+    printf("zero");
+    else if(q>0)
+    printf("positive");
+    else
+    printf("negative");
+    
+}
