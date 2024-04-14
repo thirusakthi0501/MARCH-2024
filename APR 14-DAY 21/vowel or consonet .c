2882746060cebe5408b69195ffc3f2f50cd5f378
thirusakthi0501 <104378608+thirusakthi0501@users.noreply.github.com>
2024-04-14 22:22:53 +0530

    Create vowel or consonet .c

---
+++ APR 14-DAY 21/vowel or consonet .c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+char q;
+scanf("%c",&q);
+if(q=='a' || q=='e'||q=='i'||q=='o'||q=='u'||q=='A'|| q=='E'|| q=='I'||q=='O'||q=='U')
+printf("vowel");
+else
+printf("Consonant");
+}
