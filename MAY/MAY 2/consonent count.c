4b50dffd89d5937f870657d8afbb889ca28da338
thirusakthi0501 <104378608+thirusakthi0501@users.noreply.github.com>
2024-05-02 21:30:32 +0530

    Create consonent count.c

---
+++ MAY/MAY 2/consonent count.c
@@ -0,0 +1,17 @@
+ Accept a string and count number of consonants in it.
+   #include <stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+int main()
+{
+char q[100],z;
+int w=0;
+scanf("%s",q);
+for(int e=0;e<strlen(q);e++){
+    z=tolower(q[e]);
+    if(z!='a'&& z!='e'&&z!='i'&&z!='o'&&z!='u')
+    w+=1;
+}
+printf("%d ",w);
+}
